c227f9ff8b108357f4cd16e3a1fe46ea52c8da26
nicnuyster <shirmer.alex@gmail.com>
2026-09-12 22:16:27 +0300

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+def IsHappy(n):
+    seen = set() #множество
+
+    while n != 1 and n not in seen:
+        seen.add(n)
+        summ = 0
+
+        for digit in str(n):
+            summ += int(digit) ** 2 
+        n = summ
+    return n == 1
+
+def FindHappy(limit):
+    result = []
+    count = 0
+    n = 1
+
+    while count < limit:
+        if IsHappy(n):
+            result.append(n)
+            count += 1
+        n += 1
+
+    return result
+
+if __name__ == "__main__":
+
+    print(FindHappy(10));
